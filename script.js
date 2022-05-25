29d907a6ea100c1e56274ac314dd9d615e9f6642
Call-me-phoenix <88521085+Call-me-phoenix@users.noreply.github.com>
2022-05-25 22:09:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const select=document.querySelector(".select")
+const options_list=document.querySelector(".options-list")
+const options=document.querySelectorAll(".option")
+//切换语言菜单得显示和隐藏
+select.addEventListener("click",()=>{
+  options_list.classList.toggle("active");
+  select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up")
+})
+//切换语言
+options.forEach((option)=>{
+option.addEventListener("click",()=>{
+  options.forEach((option)=>{option.classList.remove("selected")});
+  select.querySelector("span").innerHTML=option.innerHTML;
+  option.classList.add("selected");
+  options_list.classList.toggle("active")
+  select.querySelector(".fa-angle-up").classList.toggle("fa-angle-up")
+})
+})
